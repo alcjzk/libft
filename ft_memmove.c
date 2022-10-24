fd46285932f1b9f95a3d942354a054822f66179f
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2022-10-24 22:18:48 +0300

    memmove impl

---
+++ ft_memmove.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjaasalo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 22:04:27 by tjaasalo          #+#    #+#             */
+/*   Updated: 2022/10/24 22:17:46 by tjaasalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (src < dst)
+	{
+		while (--len >= 0)
+			((char *)dst)[len] = ((char *)src)[len];
+		return (dst);
+	}
+	else
+		return (ft_memcpy(dst, src, len));
+}
