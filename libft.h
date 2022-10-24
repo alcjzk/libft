21eab4e799526a754dc3fb7c5e3ecae55e744233
Tuomas Jaaasalo <tuomas.jaasalo@alceon.fi>
2022-10-24 15:32:36 +0300

    included header for size_t

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:00:06 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/10/24 15:20:41 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/10/24 15:31:40 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
