ee182444cef983a3ba2f7e66c1ef072e944274ad
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2022-10-25 11:10:04 +0300

    strncmp impl

---
+++ ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 11:00:50 by tjaasalo          #+#    #+#             */
+/*   Updated: 2022/10/25 11:09:35 by tjaasalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (*s1 && n-- > 0)
+	{
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
