d37add75204a30fde12dfc149d4049cf38b8d10c
Tuomas Jaaasalo <tuomas.jaasalo@alceon.fi>
2022-10-26 20:50:18 +0300

    strmapi impl

---
+++ ft_strmapi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 20:37:56 by tjaasalo          #+#    #+#             */
+/*   Updated: 2022/10/26 20:49:23 by tjaasalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Applies the function ’f’ to each character of the string ’s’, and passing
+**	its index as first argument to create a new string (with malloc(3))
+**	resulting from successive applications of ’f’.
+**	
+**	Return value:
+**	- The string created from the successive applications of ’f’.
+**	- Returns NULL if the allocation fails.
+*/
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = ft_strlen(s);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	while (i < len)
+	{
+		result[i] = f((unsigned int)i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
