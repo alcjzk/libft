0b75046c40454f5b03e7dc9a87792c5a795939dd
Tuomas Jaaasalo <tuomas.jaasalo@alceon.fi>
2022-10-26 20:37:12 +0300

    itoa impl

---
+++ ft_itoa.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 19:34:30 by tjaasalo          #+#    #+#             */
+/*   Updated: 2022/10/26 20:36:21 by tjaasalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_itoa_strrev(char *s)
+{
+	size_t	len;
+	size_t	i;
+	size_t	max;
+	char	tmp;
+
+	i = 0;
+	len = ft_strlen(s);
+	max = len-- / 2;
+	while (i <= max)
+	{
+		tmp = s[len];
+		s[len--] = s[i];
+		s[i++] = tmp;
+	}
+	return (s);
+}
+
+static int	ft_itoa_countdigits(int n)
+{
+	int	count;
+
+	count = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+/*
+**	Allocates (with malloc(3)) and returns a string representing the integer
+**	received as an argument. Negative numbers must be handled.
+**
+**	Return value:
+**	- The string representing the integer.
+**	- NULL if the allocation fails.
+*/
+char	*ft_itoa(int n)
+{
+	int		negative;
+	char	*result;
+	int		i;
+
+	i = 0;
+	negative = 0;
+	if (n < 0)
+		negative = 1;
+	result = malloc(sizeof(char) * ft_itoa_countdigits(n) + 1 + negative);
+	if (!result)
+		return (NULL);
+	if (negative)
+	{
+		result[i++] = (char)(-(n % 10) + 48);
+		n = -(n / 10);
+	}
+	while (n > 0)
+	{
+		result[i++] = (char)(n % 10 + 48);
+		n /= 10;
+	}
+	if (negative)
+		result[i++] = '-';
+	result[i] = '\0';
+	return (ft_itoa_strrev(result));
+}
