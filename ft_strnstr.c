aba04fbe54156a60c1acdf1c7c2da8aa02b6b511
Tuomas Jaaasalo <tuomas.jaasalo@alceon.fi>
2022-10-31 15:34:03 +0200

    fixed conflicting behaviour to libc version

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:02:07 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/10/29 02:40:25 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/10/31 15:31:45 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (len == 0 || haystack == needle)
+	if (len == 0 && *needle)
+		return (NULL);
+	if (haystack == needle || len == 0)
 		return ((char *)haystack);
 	while (haystack[i] && i < len)
 	{
