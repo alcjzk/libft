4ce80883b88362ed67caa02dbda9d32aa6c71e13
Tuomas <tuomas.jaasalo@alceon.fi>
2022-11-02 17:10:10 +0200

    fix bug in compare when one of the strings ends

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 11:00:50 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/10/27 14:27:33 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/11/02 16:58:10 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!*s1 || !*s2)
-		return ((unsigned char)*s1 - (unsigned char)*s2);
-	while (*s1 && n-- > 0)
+	while (--n < (size_t)-1 && *s1 && *s2)
 	{
 		if ((unsigned char)*s1 != (unsigned char)*s2)
 			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 	}
+	if (n < (size_t)-1)
+		return ((unsigned char)*s1 - (unsigned char)*s2);
 	return (0);
 }
