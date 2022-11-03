0f44b8e8fd35ca34308847472ed1b41069f7c7df
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2022-11-03 14:44:21 +0200

    fix #1

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:02:07 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/11/02 16:58:26 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/11/03 14:37:47 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		if (!haystack[i])
 			break ;
 		if (haystack[i++] != needle[j++])
+		{
+			i = i - (j - 1);
 			j = 0;
+		}
 	}
 	return (NULL);
 }
