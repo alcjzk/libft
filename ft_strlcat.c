d7d1137191ba4aaa5da5644acd191977dc7efac6
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2022-11-03 17:11:05 +0200

    fix #4 & removed nullptr check for dst as per libc

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,21 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 10:00:43 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/10/31 14:57:34 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/11/03 17:06:41 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	ft_strnlen(const char *s, size_t n)
+{
+	size_t	len;
+
+	len = 0;
+	while (n-- > 0 && s[len])
+		len++;
+	return (len);
+}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -19,9 +29,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	max;
 
-	dst_len = 0;
-	if (dst)
-		dst_len = ft_strlen(dst);
+	dst_len = ft_strnlen(dst, dstsize);
 	src_len = ft_strlen(src);
 	if (dst_len >= dstsize)
 		return (src_len + dstsize);
