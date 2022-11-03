84523f418731efa805dcecadd090a831d4bd9469
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2022-11-03 19:22:16 +0200

    fix #5, corrected calloc behaviour on argument edge cases

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:37:13 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/10/26 12:45:44 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/11/03 19:07:48 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*result;
 	size_t	size_full;
 
+	if (!count || !size)
+		return (ft_calloc(1, 1));
 	size_full = count * size;
+	if (count / size != size_full)
+		return (NULL);
 	result = malloc(size_full);
 	if (result)
 		ft_bzero(result, size_full);
