79025dfa9c31a486594444c8f9831f6026733e2b
Tuomas <tuomas.jaasalo@alceon.fi>
2022-12-04 13:04:58 +0200

    Fix leak & malloc size on edge cases

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:25:59 by tjaasalo          #+#    #+#             */
-/*   Updated: 2022/11/03 15:37:17 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2022/12/04 13:04:05 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	s_len = ft_strlen(s);
 	size = len + 1;
+	if (s_len < len)
+		size = s_len + 1;
+	if (start > s_len)
+		return (ft_strdup(""));
 	result = malloc(sizeof(char) * size);
 	if (!result)
 		return (NULL);
-	if (start > s_len)
-		return (ft_strdup(""));
 	ft_strlcpy(result, &s[start], size);
 	return (result);
 }
