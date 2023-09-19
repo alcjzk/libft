afcedfdac303715ef655e1664ad691fbc9441520
Tuomas <tuomas.jaasalo@alceon.fi>
2023-09-19 16:54:57 +0300

    Removed illegal define

--- inc/bool.h
+++ inc/bool.h
@@ -6,14 +6,13 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 08:17:31 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/09/19 16:53:36 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/09/19 16:54:51 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BOOL_H
 # define BOOL_H
 
-# define BOOL _Bool
 # define TRUE 1
 # define FALSE 0
 
